cde443003165d62b877802a309b7cd1121bf4c9b
samir-byte <kiranpoudel28@gmail.com>
2024-09-02 23:38:53 +0545

    hotfix: deposit wallet address

--- src/app/_lib/constants.ts
+++ src/app/_lib/constants.ts
@@ -1,5 +1,6 @@
-export const HOST_FEE=50
-export const DISPUTE_FEE=50
-export const WITHDRAWL_AMOUNT=0.97
-export const MIN_PREDICTION_AMOUNT=10
-export const DEPOSIT_WALLET="5G3nNt5W44KUnygyRVLHxxbkZCYyYSsUJAbZZuQ2SzNia5jF"
+export const HOST_FEE = 50;
+export const DISPUTE_FEE = 50;
+export const WITHDRAWL_AMOUNT = 0.97;
+export const MIN_PREDICTION_AMOUNT = 10;
+export const DEPOSIT_WALLET =
+  "5FsidgwJ1afHToksmUeTUSGt7TR4vRa6veYE15F7X3vaRbKk";
